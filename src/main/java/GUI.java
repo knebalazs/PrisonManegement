8c100ef97b9023c8807780e0d54ffed2b9283a3e
Kiss Németh Endre Balázs <knebalazs@yahoo.com>
2022-10-02 15:47:40 +0300

    prisoners can be added and deleted + logout feature

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,10 +1,13 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +16,7 @@ import java.util.ResourceBundle;
 public class GUI implements Initializable {
 
     private String selectedPrison;
+    private String currentUser;
 
     @FXML
     private ListView<Integer> PrisonerID_list;
@@ -43,6 +47,12 @@ public class GUI implements Initializable {
     private TextField Cell_Number;
     @FXML
     private ChoiceBox<String> Crime;
+    @FXML
+    private Label loginStatusLabel;
+    @FXML
+    private Label selectedPrisonLabel;
+    @FXML
+    private Button logOutButton;
 
     private final List<Integer> pids = new ArrayList<>();
     private final List<String> fnlist = new ArrayList<>();
@@ -55,12 +65,99 @@ public class GUI implements Initializable {
 
 
     @FXML
-    void addPrisoner(){
-        //TODO
+    void addPrisoner() throws IOException {
+        int Id = Integer.parseInt(PrisonerID.getText());
+        String fName = Prisoner_FN.getText();
+        String lName = Prisoner_LN.getText();
+        String entranceDate = String.valueOf(EntranceDate.getValue());
+        String releaseDate = String.valueOf(ReleaseDate.getValue());
+        String cellNumber = String.valueOf(Integer.parseInt(Cell_Number.getText()));
+        String crime = Crime.getValue();
+
+        BufferedWriter prisonerFile = new BufferedWriter(new FileWriter("src/data/prisoners.txt", true));
+        prisonerFile.append(selectedPrison + "," + Id + "," + fName + "," + lName + "," + entranceDate + "," + releaseDate  + "," + cellNumber + "," + crime + "\n");
+        prisonerFile.close();
+        refreshPrisonerList();
+
+        Alert alertwindow = new Alert(Alert.AlertType.INFORMATION);
+        alertwindow.setTitle("Information");
+        alertwindow.setContentText("Prisoner added successfully");
+        alertwindow.showAndWait();
+
+
     }
     @FXML
-    void deletePrisoner(){
-        //TODO
+    void deletePrisoner() throws IOException {
+        boolean prisonerWasFound = false;
+        int Id = Integer.parseInt(PrisonerID.getText());
+        String fName = Prisoner_FN.getText();
+        String lName = Prisoner_LN.getText();
+        String entranceDate = String.valueOf(EntranceDate.getValue());
+        String releaseDate = String.valueOf(ReleaseDate.getValue());
+        String cellNumber = String.valueOf(Integer.parseInt(Cell_Number.getText()));
+        String crime = Crime.getValue();
+
+        File inputFile = new File("src/data/prisoners.txt");
+        File tempFile = new File("src/data/prisonersTemp.txt");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+        String lineToRemove = (selectedPrison + "," + Id + "," + fName + "," + lName + "," + entranceDate + "," + releaseDate  + "," + cellNumber + "," + crime);
+        String currentLine;
+
+        while((currentLine = reader.readLine()) != null) {
+            // trim newline when comparing with lineToRemove
+            String trimmedLine = currentLine.trim();
+            if(trimmedLine.equals(lineToRemove)){
+                prisonerWasFound = true;
+                continue;
+            }
+            writer.write(currentLine + System.getProperty("line.separator"));
+        }
+
+        if (prisonerWasFound){
+            Alert alertwindow = new Alert(Alert.AlertType.INFORMATION);
+            alertwindow.setTitle("Information");
+            alertwindow.setContentText("Prisoner deleted successfully");
+            alertwindow.showAndWait();
+        }
+        else{
+            Alert alertwindow = new Alert(Alert.AlertType.WARNING);
+            alertwindow.setTitle("Information");
+            alertwindow.setContentText("Prisoner was not found!");
+            alertwindow.showAndWait();
+        }
+        writer.close();
+        reader.close();
+        inputFile.delete();
+        boolean successful = tempFile.renameTo(inputFile);
+        refreshPrisonerList();
+        System.out.println(successful);
+
+    }
+
+    @FXML
+    private void changePrison(){
+
+    }
+
+    @FXML
+    private void logOut() throws IOException {
+        Image img = new Image("/icons/login_icon.png");
+        Stage stage = new Stage();
+
+        Stage stage2 = (Stage) logOutButton.getScene().getWindow();
+        stage2.close();
+
+
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/LOGIN.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Log in");
+        stage.getIcons().add(img);
+        stage.setScene(scene);
+        stage.show();
+
     }
 
 
@@ -102,18 +199,64 @@ public class GUI implements Initializable {
         return data;
     }
 
+    public void refreshPrisonerList() throws IOException {
+        pids.clear();
+        fnlist.clear();
+        lnlist.clear();
+        edlist.clear();
+        rdlist.clear();
+        celnum.clear();
+        crimelist.clear();
+        PrisonerID_list.getItems().clear();
+        PrisonerFN_list.getItems().clear();
+        PrisonerLN_list.getItems().clear();
+        PrisonerED_list.getItems().clear();
+        PrisonerRD_list.getItems().clear();
+        PrisonerCN_list.getItems().clear();
+        PrisonerCrime_list.getItems().clear();
+
+        fillListOfPrisoners();
+        PrisonerID_list.getItems().addAll(pids);
+        PrisonerFN_list.getItems().addAll(fnlist);
+        PrisonerLN_list.getItems().addAll(lnlist);
+        PrisonerED_list.getItems().addAll(edlist);
+        PrisonerRD_list.getItems().addAll(rdlist);
+        PrisonerCN_list.getItems().addAll(celnum);
+        PrisonerCrime_list.getItems().addAll(crimelist);
+    }
+
+    public void setCrimes() throws IOException {
+        BufferedReader in = null;
+        FileReader fr = null;
+        List<String> data = new ArrayList<String>();
+
+        try {
+            fr = new FileReader("src/data/crimes.txt");
+            in = new BufferedReader(fr);
+            String str;
+            while ((str = in.readLine()) != null) {
+                data.add(str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            in.close();
+            fr.close();
+        }
+        Crime.getItems().addAll(data);
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        currentUser = LoginController.username;
         selectedPrison = LoginController.selectedPrison;
+        loginStatusLabel.setText(currentUser);
+        selectedPrisonLabel.setText(selectedPrison);
+
         try {
-            fillListOfPrisoners();
-            PrisonerID_list.getItems().addAll(pids);
-            PrisonerFN_list.getItems().addAll(fnlist);
-            PrisonerLN_list.getItems().addAll(lnlist);
-            PrisonerED_list.getItems().addAll(edlist);
-            PrisonerRD_list.getItems().addAll(rdlist);
-            PrisonerCN_list.getItems().addAll(celnum);
-            PrisonerCrime_list.getItems().addAll(crimelist);
+            setCrimes();
+            refreshPrisonerList();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
